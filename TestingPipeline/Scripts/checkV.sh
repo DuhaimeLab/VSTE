7534f907839ee87ff0cb3c33ce1c23824a74a63f
hegartybr <beh55@cornell.edu>
2022-08-23 12:31:52 -0400

    Create checkV.sh

---
+++ TestingPipeline/Scripts/checkV.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo start
+echo $1
+samplename="/scratch/duhaimem_root/duhaimem/shared_data/VSTE/TestingSet/metagenomic_testing_set_${1}.fna"
+database="/home/hegartyb/checkv-db-v0.6/"
+threads=4
+
+echo contamination
+checkv contamination $samplename ./ -t $threads -d $database
+echo completeness
+checkv completeness $samplename ./ -t $threads -d $database
+echo complete genomes
+checkv complete_genomes $samplename ./
+echo quality summary
+checkv quality_summary $samplename ./
+
+echo done
