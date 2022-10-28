7d0d9a7a99f76f880ad2d5afb5bf66ec3454535e
Bridget Hegarty <beh55@cornell.edu>
2022-10-28 12:11:56 -0400

    Updating rules with different viral types from VS2.

--- TestingPipeline/Scripts/MergingScripts/merge_virsorter2.py
+++ TestingPipeline/Scripts/MergingScripts/merge_virsorter2.py
@@ -6,7 +6,7 @@ create a list of all the vs2 viral contigs
 with open("/scratch/duhaimem_root/duhaimem/shared_data/VSTE/TestingSetToolOutputs/testing_sets_virsorter2_output.tsv", 'wt') as outfile:
     j=0
     for index in range(1,11):
-        filename="/scratch/duhaimem_root/duhaimem/shared_data/VSTE/TestingSetToolOutputs/Testing_Set_%s/final-viral-score.tsv" %(index)
+        filename="/scratch/duhaimem_root/duhaimem/shared_data/VSTE/RunningToolsOnTestingSet/VS2/VS2_%s/final-viral-score.tsv" %(index)
         with open(filename, 'rt') as v:
             if (j==0):
                 i=0
